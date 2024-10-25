37b92cd1b26c7789218d8792e147c7c8f2adec1e
einhornimmond <info@einhornimmond.de>
2024-10-25 14:57:58 +0200

    missing files

--- src/models/GradidoCreation.ts
+++ src/models/GradidoCreation.ts
@@ -1,8 +1,11 @@
+import { Type } from 'class-transformer'
 import { TimestampSeconds } from './TimestampSeconds'
 import { TransferAmount } from './TransferAmount'
 
 export class GradidoCreation {
+  @Type(() => TransferAmount)
   public recipient: TransferAmount
+  @Type(() => TimestampSeconds)
   public targetDate: TimestampSeconds
 
   public constructor(recipient: TransferAmount, targetDate: TimestampSeconds) {
